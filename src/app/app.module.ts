8f56f6fe83db2dea6b89108cb68d2f4804f9861f
Glenis Tivari <g.tivari@sogetel.it>
2024-01-12 10:19:56 -0800

    add fonts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { TodoListModule } from './todo-list/todo-list.module';
 
 @NgModule({
